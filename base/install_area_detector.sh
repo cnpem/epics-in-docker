46ae0c0dd0f5d8dd07d37820913c46aacb7e170d
Henrique F. Simoes <henrique.simoes@lnls.br>
2023-09-11 11:07:35 -0300

    base: add NDSSCPimega areaDetector plugin.
    This plugin computes the geometric restoration for Pimega detectors
    based on the LNLS ssc-pimega library, and allows Pimega IOC to provide
    coherent data to users.
    
    Even though ADPimega is not yet supported, this plugin can still be used
    with ADSimDetector for debugging purposes. Furthermore, it makes it
    possible to build ADPimega (and its IOC) out of epics-in-docker, but
    still using NDSSCPimega from base.

--- base/install_area_detector.sh
+++ base/install_area_detector.sh
@@ -2,6 +2,8 @@
 
 set -ex
 
+. /opt/epics/install-functions.sh
+
 git clone --depth 1 --branch ${AREA_DETECTOR_VERSION} \
     https://github.com/areaDetector/areaDetector
 
@@ -96,3 +98,18 @@ git apply --directory ADSupport ${EPICS_MODULES_PATH}/nanohttp_stream.patch
 
 make -j${JOBS}
 make clean
+
+cd ..
+
+git clone --depth 1 --branch ${LIBSSCPIMEGA_VERSION} \
+    https://github.com/cnpem/ssc-pimega
+
+make -C ssc-pimega/c install
+
+install_github_module cnpem NDSSCPimega NDSSCPIMEGA $NDSSCPIMEGA_VERSION "
+EPICS_BASE = ${EPICS_BASE_PATH}
+
+ASYN=${EPICS_MODULES_PATH}/asyn
+AREA_DETECTOR=${EPICS_MODULES_PATH}/areaDetector
+ADCORE=${EPICS_MODULES_PATH}/areaDetector/ADCore
+"
