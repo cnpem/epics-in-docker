109a60a99c65efe908713ee0e450e6fe151bd48b
Henrique F. Simoes <henrique.simoes@lnls.br>
2023-10-03 10:24:05 -0300

    base: extract install functions to shared file.
    These installation functions can be used in other scripts. This
    simplifies their logic through reuse and centralizes standardization.

--- base/install_modules.sh
+++ base/install_modules.sh
@@ -2,43 +2,7 @@
 
 set -ex
 
-download_github_module() {
-    github_org=$1
-    module_name=$2
-    tag=$3
-
-    lnls-get-n-unpack -l https://github.com/$github_org/$module_name/archive/refs/tags/$tag.tar.gz
-
-    mv $module_name-$tag $module_name
-}
-
-install_module() {
-    module_name=$1
-    dependency_name=$2
-    release_content="$3"
-
-    cd $module_name
-    echo "$release_content" > configure/RELEASE
-
-    make -j${JOBS} install
-    make clean
-
-    cd -
-
-    echo ${dependency_name}=${EPICS_MODULES_PATH}/${module_name} >> ${EPICS_MODULES_PATH}/../RELEASE
-}
-
-# Install module from GitHub tagged versions or URL
-install_github_module() {
-    github_org=$1
-    module_name=$2
-    dependency_name=$3
-    tag=$4
-    release_content="$5"
-
-    download_github_module $github_org $module_name $tag
-    install_module $module_name $dependency_name "$release_content"
-}
+. /opt/epics/install-functions.sh
 
 echo EPICS_BASE=${EPICS_BASE_PATH} > ${EPICS_MODULES_PATH}/../RELEASE
 
