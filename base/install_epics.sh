6322f9601df2986810bbd4a3137fd77ce4671217
Érico Nogueira <erico.rolim@lnls.br>
2023-11-27 08:38:11 -0300

    base: add patch to allow building with musl.

--- base/install_epics.sh
+++ base/install_epics.sh
@@ -7,4 +7,6 @@ set -ex
 lnls-get-n-unpack -l https://epics-controls.org/download/base/base-${EPICS_BASE_VERSION}.tar.gz
 mv base-${EPICS_BASE_VERSION} ${EPICS_BASE_PATH}
 
+patch -d ${EPICS_BASE_PATH} -Np1 < backport-epics-base-musl.patch
+
 install_module base EPICS_BASE
