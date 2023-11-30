d7067fe2c28d3c54e9e590060f83a5ebeb64951d
Érico Nogueira <erico.rolim@lnls.br>
2023-11-30 11:29:52 -0300

    base: build epics-base with GCC_PIPE=YES.
    This can speed up the build by avoiding usage of temporary files.

--- base/install_epics.sh
+++ base/install_epics.sh
@@ -7,6 +7,8 @@ set -ex
 lnls-get-n-unpack -l https://epics-controls.org/download/base/base-${EPICS_BASE_VERSION}.tar.gz
 mv base-${EPICS_BASE_VERSION} ${EPICS_BASE_PATH}
 
+echo GCC_PIPE=YES >> ${EPICS_BASE_PATH}/configure/CONFIG_SITE
+
 patch -d ${EPICS_BASE_PATH} -Np1 < backport-epics-base-musl.patch
 
 install_module base EPICS_BASE
