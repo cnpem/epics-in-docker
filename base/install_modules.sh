28644064b1004ee3955c11c30e92ddf59c007bf4
Henrique F. Simoes <henrique.simoes@lnls.br>
2024-01-08 17:35:52 -0300

    base: use RELEASE to determine module paths.
    Right after being installed, each module adds its own path to the
    RELEASE file. Therefore, it is possible to use the RELEASE file as the
    source of truth for our dependencies' paths. Since a module cannot be
    built without its dependency already built, this does not limit our
    installation scripts.
    
    This removes duplication of several module paths, which may now be
    changed more easily.
    
    If a non-existent dependency is provided and errexit (-e) bash option is
    set, the script will fail during the get_module_path's grep call due to
    its non-zero exit code.

--- base/install_modules.sh
+++ base/install_modules.sh
@@ -5,65 +5,60 @@ set -ex
 . /opt/epics/install-functions.sh
 
 install_github_module mdavidsaver pvxs PVXS $PVXS_VERSION "
-EPICS_BASE = ${EPICS_BASE_PATH}
+EPICS_BASE
 "
 
 # Build seq first since it doesn't depend on anything
 lnls-get-n-unpack -l "https://static.erico.dev/seq-$SEQUENCER_VERSION.tar.gz"
 mv seq-$SEQUENCER_VERSION seq
 install_module seq SNCSEQ "
-EPICS_BASE = ${EPICS_BASE_PATH}
+EPICS_BASE
 "
 
 install_github_module epics-modules calc CALC $CALC_VERSION "
-EPICS_BASE = ${EPICS_BASE_PATH}
-
-SNCSEQ = ${EPICS_MODULES_PATH}/seq
+EPICS_BASE
+SNCSEQ
 "
 
 # Build asyn without seq since it's only needed for testIPServer
 install_github_module epics-modules asyn ASYN $ASYN_VERSION "
-EPICS_BASE = ${EPICS_BASE_PATH}
-
-CALC = ${EPICS_MODULES_PATH}/calc
+EPICS_BASE
+CALC
 "
 
 install_github_module paulscherrerinstitute StreamDevice STREAM $STREAMDEVICE_VERSION "
-EPICS_BASE = ${EPICS_BASE_PATH}
-
-ASYN = ${EPICS_MODULES_PATH}/asyn
-CALC = ${EPICS_MODULES_PATH}/calc
+EPICS_BASE
+ASYN
+CALC
 "
 
 install_github_module epics-modules busy BUSY $BUSY_VERSION "
-EPICS_BASE = ${EPICS_BASE_PATH}
-
-ASYN = ${EPICS_MODULES_PATH}/asyn
+EPICS_BASE
+ASYN
 "
 
 install_github_module epics-modules autosave AUTOSAVE $AUTOSAVE_VERSION "
-EPICS_BASE = ${EPICS_BASE_PATH}
+EPICS_BASE
 "
 
 install_github_module epics-modules sscan SSCAN $SSCAN_VERSION "
-EPICS_BASE = ${EPICS_BASE_PATH}
-
-SNCSEQ = ${EPICS_MODULES_PATH}/seq
+EPICS_BASE
+SNCSEQ
 "
 
 download_github_module ChannelFinder recsync $RECCASTER_VERSION
 install_module recsync/client RECCASTER "
-EPICS_BASE = ${EPICS_BASE_PATH}
+EPICS_BASE
 "
 
 install_github_module epics-modules ipac IPAC $IPAC_VERSION "
-EPICS_BASE = ${EPICS_BASE_PATH}
+EPICS_BASE
 "
 
 install_github_module epics-modules caPutLog CAPUTLOG $CAPUTLOG_VERSION "
-EPICS_BASE = ${EPICS_BASE_PATH}
+EPICS_BASE
 "
 
 install_github_module brunoseivam retools RETOOLS $RETOOLS_VERSION "
-EPICS_BASE = ${EPICS_BASE_PATH}
+EPICS_BASE
 "
