1a83a50922eb637dce4fe756642779d8ba57f6d3
Henrique F. Simoes <henrique.simoes@lnls.br>
2024-04-04 14:29:33 -0300

    ioc: prune modules from non-static builds.
    Remove EPICS modules that do not have a dynamic library linked to any
    executables in the target IOC. This shrinks the final image size by
    removing useless artifacts.
    
    The clean up phase must be executed in the build stage (before the final
    copy), otherwise COPY layer caching would still make the image size big.
    This implies `no-build` target needs to copy from a pruned version of
    the base image, which is a new stage.
    
    The list of modules to be removed is taken from the RELEASE file, so
    that it contains a valid mapping of all modules that have been
    installed. EPICS base is assumed to be always needed, even though some
    binaries in it can certainly be thrown away given the linkage of the IOC
    (for instance, PVAccess binaries when the IOC uses only Channel Access).
    
    Linkage information is taken from ldd(1), which assumes the inspected
    binaries are safe to be potentially executed.

---
+++ base/lnls-prune-artifacts.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+set -eu
+
+target_paths=$@
+
+
+get_used_epics_modules() {
+    targets=$@
+
+    executables=$(find $targets -type f -executable -print)
+    libs="$(ldd $executables 2>/dev/null)"
+
+    modules=()
+    epics_libs=$(echo "$libs" | grep -oE "$EPICS_MODULES_PATH/.*/*.so")
+    for lib in $epics_libs; do
+        module=${lib%/lib/*/*lib*so}
+        echo $module
+    done | sort | uniq
+}
+
+get_all_epics_modules() {
+    release_defs=$(cat ${EPICS_RELEASE_FILE} | grep = | cut -d'=' -f 2)
+
+    modules="$(echo "$release_defs" | grep $EPICS_MODULES_PATH)"
+
+    echo "$modules"
+}
+
+remove_unused_epics_modules() {
+    targets=$@
+
+    used_modules=$(get_used_epics_modules $targets)
+
+    for module in $(get_all_epics_modules); do
+        if [[ -d $module && ! $used_modules =~ "$module" ]]; then
+            size=$(du -hs $module | cut -f 1)
+
+            echo "Removing module '$module' ($size)..."
+            rm -rf $module
+        fi
+    done
+}
+
+remove_unused_epics_modules $target_paths
