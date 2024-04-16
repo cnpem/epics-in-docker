10b19db5e0cb716dc227ce2594eb932570c80897
Henrique F. Simoes <henrique.simoes@lnls.br>
2024-04-16 08:41:20 -0300

    ioc: prune module directories from non-static builds.
    Modules may contain several artifacts, including configuration files,
    graphical interface files and other repository artifacts that do not
    need to be in the IOC image.
    
    Remove them all except the ones containing EPICS database (`.db` and
    `.template`) or autosave requirement (`.req`) files, besides shared
    libraries. `bin` is also removed, as only $REPONAME and $RUNDIR should
    contain target executables.

--- base/lnls-prune-artifacts.sh
+++ base/lnls-prune-artifacts.sh
@@ -19,6 +19,14 @@ get_linked_libraries() {
     done | sort -u
 }
 
+find_shared_libs() {
+    search_paths=$@
+
+    libs=$(find $search_paths -type f -executable -name *.so*)
+
+    echo "$libs" | grep -E "*.so(.[0-9]+)*$" | sort -u
+}
+
 get_used_epics_modules() {
     targets=$@
 
@@ -60,9 +68,7 @@ remove_unused_shared_libs() {
     targets=$@
 
     used_libs=$(get_linked_libraries $targets)
-
-    remove_libs=$(find /opt /usr/local -type f -executable -name *.so*)
-    remove_libs=$(echo "$remove_libs" | grep -E "*.so(.[0-9]+)*$" | sort -u)
+    remove_libs=$(find_shared_libs /opt /usr/local)
 
     for lib in $used_libs; do
         remove_libs=$(echo "$remove_libs" | grep -v $lib)
@@ -76,7 +82,27 @@ remove_unused_shared_libs() {
     done
 }
 
-remove_unused_epics_modules() {
+prune_module_dirs() {
+    module=$1
+
+    keep_paths="
+        $(find_shared_libs $module)
+        $(find $module -type f -regex ".*\.\(db\|template\|req\)" -printf "%h\n" | sort -u)
+    "
+
+    for candidate in $(find $module -type d); do
+        [ -d $candidate ] || continue
+
+        if [[ ! $keep_paths =~ "$candidate".* ]]; then
+            size=$(du -hs $candidate | cut -f 1)
+
+            printf "Removing directory '$candidate' ($size)...\n"
+            rm -rf $candidate
+        fi
+    done
+}
+
+clean_up_epics_modules() {
     targets=$@
 
     used_modules=$(get_used_epics_modules $targets)
@@ -87,10 +113,12 @@ remove_unused_epics_modules() {
 
             echo "Removing module '$module' ($size)..."
             rm -rf $module
+        elif [ -d $module/lib ]; then
+            prune_module_dirs $module
         fi
     done
 }
 
-remove_unused_epics_modules $@
+clean_up_epics_modules $@
 remove_static_libs /opt
 remove_unused_shared_libs $@
