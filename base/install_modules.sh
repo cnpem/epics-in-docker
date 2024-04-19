7c9a18a487ca6a9e03df4978fca0c911277fdf2b
Guilherme Rodrigues de Lima <guilherme.lima@lnls.br>
2024-04-19 16:31:57 -0300

    base: add ether_ip IOC and module.
    The module was installed in the modules directory, because there can be
    other IOCs that depend on the ether_ip module, even though it ships an
    IOC of its own.

--- base/install_modules.sh
+++ base/install_modules.sh
@@ -61,3 +61,7 @@ EPICS_BASE
 install_from_github brunoseivam retools RETOOLS $RETOOLS_VERSION "
 EPICS_BASE
 "
+
+install_from_github -i epics-modules ether_ip ETHER_IP $ETHER_IP_VERSION "
+EPICS_BASE
+"
