cnpem/epics-in-docker

commit 8f02abc2047d5087ce69ed032b4e42006ec56605

ioc: install user-specified build and runtime tarball packages.
This makes it possible to install software not packaged in the
distribution repositories, such as closed-source local packages.
Packages can be installed during the build-stage, since the IOC might
require a given library also at compile time.

`ldconfig` is executed at the end to refresh the dynamic linker library
resolution cache, which is required to identify new libraries installed
with lnls-get-n-unpack. `ldconfig` is not required for the build stage,
as dynamic link will take place only during the binary runtime.

Dockerfile
@@ -7,6 +7,7 @@ FROM debian:${DEBIAN_VERSION}-slim AS base
 ARG RUNDIR
 ARG ENTRYPOINT=/bin/bash
 ARG RUNTIME_PACKAGES
+ARG RUNTIME_TAR_PACKAGES
 
 RUN apt update -y && \
     apt install -y --no-install-recommends \
@@ -14,9 +15,14 @@ RUN apt update -y && \
         busybox \
         netcat-openbsd \
         procserv \
+        wget \
         $RUNTIME_PACKAGES && \
     apt clean && \
     rm -rf /var/lib/apt/lists/*
+
+COPY --from=build-image /usr/local/bin/lnls-get-n-unpack /usr/local/bin/lnls-get-n-unpack
+RUN lnls-get-n-unpack -r $RUNTIME_TAR_PACKAGES && \
+    ldconfig
 
 WORKDIR ${RUNDIR}
 
@@ -34,8 +40,10 @@ FROM build-image AS build-stage
 
 ARG REPONAME
 ARG BUILD_PACKAGES
+ARG BUILD_TAR_PACKAGES
 
 RUN if [ -n "$BUILD_PACKAGES" ]; then apt update && apt install $BUILD_PACKAGES; fi
+RUN lnls-get-n-unpack -r $BUILD_TAR_PACKAGES
 
 WORKDIR /opt/${REPONAME}
 

commit b7fdf8da275da3ac5f3458a027098daf7bc85bdf

ioc: install busybox symbolic links.
Make all busybox goodies directly available in PATH, to avoid having to
call them from the multicall binary.

This allows the removal of the wget package from the build image, since
busybox provides its own version, and it's become simpler to use that.
However, the version packaged by Debian does not implement the `-o`
flag, requiring us to use shell redirection instead.

Fixes: 8f02abc2047d5087ce69ed032b4e42006ec56605

Dockerfile
@@ -16,10 +16,9 @@ RUN apt update -y && \
         busybox \
         netcat-openbsd \
         procserv \
-        wget \
         $([ -n "$RUNTIME_PIP_PACKAGES" ] && echo pip) \
         $RUNTIME_PACKAGES && \
-    ln -s /bin/busybox /usr/bin/unzip && \
+    busybox --install && \
     apt clean && \
     rm -rf /var/lib/apt/lists/*
 

base/lnls-get-n-unpack.sh
@@ -18,7 +18,7 @@ for url; do
     download_dir=$(mktemp -d)
 
     echo Downloading "$url"...
-    wget -P $download_dir -o /tmp/wget.log "$url" || (cat /tmp/wget.log && false)
+    wget -P $download_dir "$url" &> /tmp/wget.log || (cat /tmp/wget.log && false)
 
     filename=$(basename $download_dir/*)
 