c3186e89c1785dbe43063aaaa356a411eabf36b9
Érico Nogueira <erico.rolim@lnls.br>
2024-08-28 13:16:52 -0300

    base: fix static linking of the COMMANDLINE_LIBRARY.
    When building statically linked binaries, it is necessary to link them
    to all libraries, so it becomes necessary for COMMANDLINE_LIBRARY to
    point to libreadline and its dependency, ncurses. The static version of
    said dependency was also missing from the container.
    
    We are repurposing a mechanism intended to support older Linux distros,
    not static linking, and care should be taken to make sure it keeps
    working when base is updated.

--- base/install_epics.sh
+++ base/install_epics.sh
@@ -10,4 +10,8 @@ mv base-${EPICS_BASE_VERSION} ${EPICS_BASE_PATH}
 patch -d ${EPICS_BASE_PATH} -Np1 < backport-epics-base-musl.patch
 patch -d ${EPICS_BASE_PATH} -Np1 < epics-base-static-linking.patch
 
+if [ -n "$COMMANDLINE_LIBRARY" ]; then
+    echo "COMMANDLINE_LIBRARY = $COMMANDLINE_LIBRARY" > ${EPICS_BASE_PATH}/configure/CONFIG_SITE.local
+fi
+
 install_module base EPICS_BASE
