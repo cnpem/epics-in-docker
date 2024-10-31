edf0d2b7483dd7a4ee996acb7463bbea2afb5538
Érico Nogueira <erico.rolim@lnls.br>
2024-10-31 12:55:15 -0300

    base: update epics-base to 7.0.8.1.
    This also made it necessary to update the OPCUA module, so we could keep
    using the prebuilt version.
    
    Since the OPCUA installation scheme was improved, manually adding the
    libopcua symlink is no longer necessary. Furthermore, in order to keep
    the image compatible with the OPCUA IOC image build, and with st.cmd
    files from users of the image, the IOC was installed to 'opcua', and the
    library to 'opcua-module'; this is transparent to users, thanks to the
    RELEASE file.

--- base/install_opcua.sh
+++ base/install_opcua.sh
@@ -7,13 +7,17 @@ set -ex
 . $EPICS_IN_DOCKER/opcua_versions.sh
 
 opcua_release_url=https://github.com/epics-modules/opcua/releases/download/v${OPCUA_VERSION}
-opcua_release_file=IOC_opcua-${OPCUA_VERSION}_Base-${EPICS_BASE_VERSION}_debian${DEBIAN_VERSION%.*}.tar.gz
+opcua_release_file=BDIST_opcua-${OPCUA_VERSION}_Base-${EPICS_BASE_VERSION}_debian${DEBIAN_VERSION%.*}.tar.gz
 lnls-get-n-unpack -l $opcua_release_url/$opcua_release_file
+rm HOW_TO.md
 
-mv binaryOpcuaIoc opcua
-install_module -i opcua OPCUA "
+mv opcuaBinaryDist opcua-module
+install_module opcua-module OPCUA "
 EPICS_BASE
 "
 
-EPICS_HOST_ARCH=`perl ${EPICS_BASE_PATH}/lib/perl/EpicsHostArch.pl`
-ln -s ${EPICS_MODULES_PATH}/opcua/{opcuaIocApp/libopcua.so.0.9,lib/${EPICS_HOST_ARCH}/libopcua.so}
+mv opcuaExampleIoc opcua
+install_module -i opcua OPCUA_IOC "
+EPICS_BASE
+OPCUA
+"
