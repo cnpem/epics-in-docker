84a9b98ce45a29a6e1e752976680a73c881658bf
marcofilho <marco.filho@ess.eu>
2024-11-20 23:08:28 +0100

    base: add help function to lnls-get-n-unpack.
    Add help function to be called when arguments are not called
    appropriately.
    
    Add call to help function when script is called with no arguments or
    with '-h' option.

--- base/lnls-get-n-unpack.sh
+++ base/lnls-get-n-unpack.sh
@@ -4,7 +4,30 @@
 
 set -eu
 
+help () {
+    echo    "lnls-get-n-unpack: download and extract archive from the network"
+    echo -e "Usage: lnls-get-n-unpack <extraction_mode> <URL1> [URL2] [...] [URLN]\n"
+    echo    "Extraction mode:"
+    echo    "  -l extracts to local directory (./)"
+    echo    "  -r extracts to root directory (/)"
+    echo    "URL:"
+    echo    "  url to download source from."
+    exit 0
+}
+
 check_arguments() {
+
+    # No arguments = call help and exit.
+    if [ "$#" -eq 0 ]; then
+        help
+    fi
+
+    # if -h is anywhere arguments list, call help and exit.
+    for arg in "$@"; do
+        if [ "$arg" == "-h" ] || [ "$arg" == "--help" ]; then
+            help
+        fi
+    done
 
     case "$1" in
         -r) dest=/ ;;
