34d09bdc2fd04c1e0852c53d6afb60c267af4179
marcofilho <marco.filho@ess.eu>
2024-11-20 22:50:59 +0100

    base: encapsulate lnls-get-n-unpack in functions.
    This script will grow a bit, so organize it before making it grow.
    Function content is indented. Functions parse_arguments and download
    are used at the end of script.
    
    Add help function to be called when script is misused. Format of
    arguments passed to script will change in future commits, so this might
    help clarify when wrong usage is made.
    
    Similar to usual ArgumentParser usage in Python, parse_arguments decides
    which arguments will be passed to download.

--- base/lnls-get-n-unpack.sh
+++ base/lnls-get-n-unpack.sh
@@ -4,30 +4,55 @@
 
 set -eu
 
-case "$1" in
-    -r) dest=/ ;;
-    -l) dest=. ;;
-    *)
-        >&2 echo "Invalid extraction mode: must be either root (-r) or local (-l)."
-        exit 1
-        ;;
-esac
+help() {
+    echo "lnls-get-n-unpack: download and extract archive from the network"
+    echo -e "Usage: lnls-get-n-unpack <extraction_mode> <URL1> [<URL2>...]\n"
+    echo "Extraction mode:"
+    echo "  -l extracts to local directory (./)"
+    echo "  -r extracts to root directory (/)"
+    echo "URL:"
+    echo "  url to download source from."
+    exit 0
+}
 
-shift
-
-for url; do
-    download_dir=$(mktemp -d)
-
-    echo Downloading "$url"...
-    wget -P $download_dir "$url" &> /tmp/wget.log || (cat /tmp/wget.log && false)
-
-    filename=$(basename $download_dir/*)
-
-    if [[ ${filename,,} == *".zip" ]]; then
-        unzip -qo $download_dir/$filename -d $dest
-    else
-        tar --no-same-owner -xf $download_dir/$filename -C $dest
+parse_arguments() {
+    if [ "$#" -eq 0 ]; then
+        help
     fi
 
-    rm -rf $download_dir /tmp/wget.log
-done
+    case "$1" in
+        -r) dest=/ ;;
+        -l) dest=. ;;
+        *)
+            echo >&2 "Invalid extraction mode: must be either root (-r) or local (-l)."
+            exit 1
+            ;;
+    esac
+
+    # Throw extraction mode argument away
+    shift
+    echo "$dest $@"
+}
+
+download() {
+    dest=$1
+    shift
+
+    for url; do
+        download_dir=$(mktemp -d)
+        echo Downloading "$url"...
+        wget -P $download_dir "$url" &> /tmp/wget.log || (cat /tmp/wget.log && false)
+
+        filename=$(basename $download_dir/*)
+        if [[ ${filename,,} == *".zip" ]]; then
+            unzip -qo $download_dir/$filename -d $dest
+        else
+            tar --no-same-owner -xf $download_dir/$filename -C $dest
+        fi
+
+        rm -rf $download_dir /tmp/wget.log
+    done
+}
+
+args=$(parse_arguments ${@})
+download ${args}
