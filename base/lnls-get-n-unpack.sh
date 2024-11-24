c5139283aeb1f35936818ac431f7d97a07c28090
marcofilho <marco.filho@ess.eu>
2024-11-24 11:42:19 +0100

    base: add actual sha256sum checking.
    lnls-get-n-unpack now uses all variables defined in .env file as
    arguments:
      - shacheck function checks the hashes.
      - Download function now iterates through pairs of arguments to get url
        and hash for each module.
      - Help function describes this usage and can now exit with error code
        as argument.
    
    install_epics, install_opcua, install_area_detector, install_modules and
    install_motor:
      - pass variables defined in .env as arguments to lnls-get-n-unpack and
        install_from_github.
    
    install_functions:
        - Add sha as argument to install_from_github and
          download_from_github. install_from_github passes sha as argument
          to download_from_github.

--- base/lnls-get-n-unpack.sh
+++ base/lnls-get-n-unpack.sh
@@ -6,13 +6,15 @@ set -eu
 
 help () {
     echo    "lnls-get-n-unpack: download and extract archive from the network"
-    echo -e "Usage: lnls-get-n-unpack <extraction_mode> <URL1> [URL2] [...] [URLN]\n"
+    echo -e "Usage: lnls-get-n-unpack <extraction_mode> <URL1> <1SHA256SUM> [URL2] [2SHA256SUM] [...] [URLN] [NSHA256SUM]\n"
     echo    "Extraction mode:"
     echo    "  -l extracts to local directory (./)"
     echo    "  -r extracts to root directory (/)"
     echo    "URL:"
     echo    "  url to download source from."
-    exit 0
+    echo    "SHA256SUM:"
+    echo    "  Reference sha256 hash to compare url download with."
+    exit $1
 }
 
 check_arguments() {
@@ -25,10 +27,11 @@ check_arguments() {
     # if -h is anywhere arguments list, call help and exit.
     for arg in "$@"; do
         if [ "$arg" == "-h" ] || [ "$arg" == "--help" ]; then
-            help
+            help 0
         fi
     done
 
+    # Check extraction mode
     case "$1" in
         -r) dest=/ ;;
         -l) dest=. ;;
@@ -37,19 +40,48 @@ check_arguments() {
         ;;
     esac
 
+    # Check if we have odd number of arguments (extraction_mode + N*url + N*sha256sum)
+    if [ $(( $# % 2 )) -ne 1 ]; then
+        >&2 echo "ERROR: Even number of arguments detected. Something is wrong."
+        help 1
+    fi
+
+}
+
+shacheck() {
+
+    download_dir=$1
+    sha=$2
+    url=$3
+
+    downloaded_file=$(find $download_dir -type f)
+    if [[ $(echo $downloaded_file | wc -w) -ne 1 ]]; then
+        echo "ERROR: Download of $url is yielding something different than one single file."
+        echo "Don't know how to proceed. Exiting..."
+        exit 1
+    fi
+
+    if ! echo $sha $downloaded_file | sha256sum -c; then
+        echo "ERROR: SHA $sha for URL $url does not match."
+        exit 1
+    fi
+
 }
 
 download () {
 
     shift # Throw extraction mode argument away
 
-    for url; do
+    while [[ $# -gt 1 ]]; do
+        url=$1
+        sha=$2
         download_dir=$(mktemp -d)
 
         echo Downloading "$url"...
         wget -P $download_dir "$url" &> /tmp/wget.log || (cat /tmp/wget.log && false)
 
         filename=$(basename $download_dir/*)
+        shacheck $download_dir $sha $url
 
         if [[ ${filename,,} == *".zip" ]]; then
             unzip -qo $download_dir/$filename -d $dest
@@ -58,6 +90,8 @@ download () {
         fi
 
         rm -rf $download_dir /tmp/wget.log
+
+        shift 2
     done
 
 }
