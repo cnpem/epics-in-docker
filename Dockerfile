cnpem/epics-in-docker

commit 39bb0d4e1d42765b67c1eda5a04d40a05b06c637

base: update to Debian 12.
And update relevant package versions.

We chose to install libtirpc3 by default, since so many IOCs depend on
Asyn and it would have complicated the IOC build for all of them.

Dockerfile
@@ -1,4 +1,4 @@
-ARG DEBIAN_VERSION=11.9
+ARG DEBIAN_VERSION=12.8
 
 FROM ghcr.io/cnpem/lnls-debian-epics-7:v0.12.0-dev AS build-image
 
@@ -15,6 +15,7 @@ COPY --from=build-image /etc/apt/apt.conf.d/90-disable-sandbox.conf /etc/apt/apt
 RUN apt update -y && \
     apt install -y --no-install-recommends \
         libreadline8 \
+        libtirpc3 \
         busybox \
         netcat-openbsd \
         procserv \

base/Dockerfile
@@ -25,6 +25,7 @@ RUN apt update -y && \
         libxml2-dev \
         libsnmp-dev \
         libssl-dev \
+        libtirpc-dev \
         re2c \
         unzip \
         wget \
@@ -55,7 +56,7 @@ WORKDIR ${EPICS_MODULES_PATH}
 
 COPY backport-ipmicomm.patch ipmicomm.patch caputlog-waveform-fix.patch $EPICS_IN_DOCKER
 COPY modules_versions.sh install_modules.sh $EPICS_IN_DOCKER
-RUN $EPICS_IN_DOCKER/install_modules.sh
+RUN NEEDS_TIRPC=YES $EPICS_IN_DOCKER/install_modules.sh
 
 COPY backport-adsupport-nanohttp.patch $EPICS_IN_DOCKER
 COPY area_detector_versions.sh install_area_detector.sh $EPICS_IN_DOCKER

base/docker-compose.yml
@@ -10,4 +10,4 @@ services:
         org.opencontainers.image.description: "EPICS base and modules build image"
       args:
         JOBS: ${JOBS:--1}
-        DEBIAN_VERSION: 11.9
+        DEBIAN_VERSION: 12.8

images/docker-compose-opcua.yml
@@ -10,4 +10,4 @@ services:
       args:
         REPONAME: opcua
         RUNDIR: /opt/epics/modules/opcua/iocBoot/iocUaDemoServer
-        RUNTIME_PACKAGES: libxml2 libssl1.1
+        RUNTIME_PACKAGES: libxml2 libssl3

commit be1d5859c68ae2e2b9eeb6daa1f1b35382d70ada

ioc: allow pip to install packages system-wide.
Since Debian 12, installing packages outside a virtual environment is an
error by default [1] as first specified by PEP 668 [2]. The rationale
for forcing this is not applicable in our context, since the container
image is dedicated to the IOC under construction, which renders it an
isolated environment of its own.

Users should still provide RUNTIME_PIP_PACKAGES that are compatible with
the system packages, not really breaking the system, similarly to what
is done for shared libraries.

[1] https://salsa.debian.org/python-team/packages/python-pip/-/commit/5aeba2464e36020a34e2bc074872771f5ef4973d
[2] https://peps.python.org/pep-0668

Fixes: 39bb0d4e1d42 (base: update to Debian 12., 2024-10-31)

Dockerfile
@@ -30,7 +30,7 @@ COPY --from=build-image /usr/local/bin/lnls-get-n-unpack /usr/local/bin/lnls-get
 RUN lnls-get-n-unpack -r $RUNTIME_TAR_PACKAGES && \
     ldconfig
 RUN if [ -n "$RUNTIME_PIP_PACKAGES" ]; then \
-        pip install $RUNTIME_PIP_PACKAGES; \
+        pip install --break-system-packages $RUNTIME_PIP_PACKAGES; \
     fi
 
 COPY --from=build-image /usr/local/bin/lnls-run /usr/local/bin/lnls-run