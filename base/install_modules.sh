22508c15e0dabe975dfcdc13803736a5f5ea8bb5
Érico Nogueira <erico.rolim@lnls.br>
2025-02-17 17:10:27 -0300

    base: add ca-gateway module.

--- base/install_modules.sh
+++ base/install_modules.sh
@@ -126,3 +126,9 @@ MCA
 install_from_github epics-modules pcas PCAS $PCAS_VERSION "
 EPICS_BASE
 "
+
+install_from_github epics-extensions ca-gateway CA_GATEWAY $CA_GATEWAY_VERSION "
+EPICS_BASE
+PCAS
+CAPUTLOG
+"
