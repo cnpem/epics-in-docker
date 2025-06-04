cnpem/epics-in-docker

commit 1ed359f7d1d7eb8fe78a774b90e58659944914c2

ioc: deduplicate build steps in different targets.
Build steps must be kept synchronized for static- and dynamic-link build
targets, which is a manual task. While this is a one-line script, it is
expected to grow, including post-build cleanup and extension of build
options beyond the project's own definitions. Deduplicate this by
extracting it to a standalone script, which is called whenever needed.

As a bonus, we get more room for writing commands in a more readable
fashion.

base/lnls-build-ioc.sh
@@ -1,0 +1,14 @@
+#!/bin/sh
+
+set -eux
+
+make distclean
+make -j ${JOBS}
+
+if [ "$SKIP_TESTS" != 1 ]; then
+  make runtests
+fi
+
+make clean
+
+make -C ${RUNDIR}

Dockerfile
@@ -101,7 +101,7 @@ ARG RUNDIR
 ARG SKIP_TESTS
 ARG SKIP_PRUNE
 
-RUN make distclean && make -j ${JOBS} && make $([ "$SKIP_TESTS" != 1 ] && echo runtests) && make clean && make -C ${RUNDIR}
+RUN lnls-build-ioc
 
 RUN if [ "$SKIP_PRUNE" != 1 ]; then lnls-prune-artifacts ${APP_DIRS} ${PWD} ${RUNDIR}; fi
 
@@ -128,7 +128,7 @@ ARG SKIP_TESTS
 
 RUN echo STATIC_BUILD=YES >> configure/CONFIG_SITE
 
-RUN make distclean && make -j ${JOBS} && make $([ "$SKIP_TESTS" != 1 ] && echo runtests) && make clean && make -C ${RUNDIR}
+RUN lnls-build-ioc
 
 
 FROM base AS static-link

base/Dockerfile
@@ -71,5 +71,6 @@ ARG DEBIAN_VERSION
 COPY opcua_versions.sh install_opcua.sh $EPICS_IN_DOCKER
 RUN $EPICS_IN_DOCKER/install_opcua.sh
 
+COPY lnls-build-ioc.sh /usr/local/bin/lnls-build-ioc
 COPY lnls-prune-artifacts.sh /usr/local/bin/lnls-prune-artifacts
 COPY lnls-run.sh /usr/local/bin/lnls-run

commit f7e8631bec547b42be6f3ad32f139197798f923c

ioc: default SKIP_TESTS to false.
When the code was extracted to a standalone script in the fixed commit,
undefined variables were enforced not to exist by using the `-u` switch.
However, SKIP_TESTS is thought to be optionally defined in the compose
file (in order to make it more concise), which means it is often
undefined. Give it the default value of false to make it optional again.
Keep the setting of failing upon usage of undefined variables, as it is
still beneficial in general.

Note that no other variable needs a default value, as we enforced RUNDIR
to be defined (because it is the argument of a required option), and
JOBS always had a default value.

Fixes: 1ed359f (ioc: deduplicate build steps in different targets., 2025-05-22)

base/lnls-build-ioc.sh
@@ -5,7 +5,7 @@ set -eux
 make distclean
 make -j ${JOBS}
 
-if [ "$SKIP_TESTS" != 1 ]; then
+if [ "${SKIP_TESTS:-0}" != 1 ]; then
   make runtests
 fi
 