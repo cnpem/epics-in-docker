1a125c613c308b3879c341d47c88d9a3f5ebcf07
Marcos Alcino Ribeiro Cussioli <marcos.cussioli@lnls.br>
2025-10-23 08:50:25 -0300

    base: add twincat-ads module
    Install twincat-ads separating modules and IOC
    
    - Recursive clone of twincat-ads to include Beckhoff ADS sources
      (download/binary doesn't bring this submodule).
    - Split twincat into two folders: ads-modules (modules) and ads-ioc (example IOC),
      and adapt configure/RELEASE (-include).

---
+++ base/install_twincat_ads.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+set -ex
+
+. $EPICS_IN_DOCKER/install-functions.sh
+. $EPICS_IN_DOCKER/epics_versions.sh
+. $EPICS_IN_DOCKER/twincat_ads_versions.sh
+
+git clone --recursive --depth 1 --branch ${TWINCAT_ADS_VERSION} \
+    https://github.com/epics-modules/twincat-ads
+
+mv twincat-ads ads-modules
+cd ads-modules
+rm -rf .git*
+
+sed -i \
+    -e 's|^include|-include|' \
+    configure/RELEASE
+
+cd ..
+
+install_module ads-modules ADS "
+EPICS_BASE
+ASYN
+"
+
+mkdir ads-ioc && cd ads-ioc
+yes ads | /opt/epics/base/bin/linux-x86_64/makeBaseApp.pl -i -t example
+cd ..
+
+cp -r ads-modules/adsExApp ads-ioc/adsExApp
+cp ads-modules/startup/* ads-ioc/iocBoot/iocads/
+rm ads-ioc/iocBoot/iocads/st.cmd
+
+install_module -i ads-ioc ADS_IOC "
+EPICS_BASE
+ASYN
+ADS
+"
