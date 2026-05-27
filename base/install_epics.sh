9ee882e2ac278da1b6101642ecc0aa8f57f306cc
Érico Nogueira <erico.rolim@lnls.br>
2026-05-27 13:58:33 -0300

    base: append to CONFIG_SITE.local by default
    This makes adding other configurations to CONFIG_SITE.local less
    error-prone.

--- base/install_epics.sh
+++ base/install_epics.sh
@@ -11,7 +11,7 @@ mv base-${EPICS_BASE_VERSION} ${EPICS_BASE_PATH}
 patch -d ${EPICS_BASE_PATH} -Np1 < $EPICS_IN_DOCKER/epics-base-static-linking.patch
 
 if [ -n "$COMMANDLINE_LIBRARY" ]; then
-    echo "COMMANDLINE_LIBRARY = $COMMANDLINE_LIBRARY" > ${EPICS_BASE_PATH}/configure/CONFIG_SITE.local
+    echo "COMMANDLINE_LIBRARY = $COMMANDLINE_LIBRARY" >> ${EPICS_BASE_PATH}/configure/CONFIG_SITE.local
 fi
 
 install_module base EPICS_BASE
