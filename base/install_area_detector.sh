cnpem/epics-in-docker

commit d480b9ab8fb963067b6b72ec64ebc36d58c38e44

base: run ADCore tests.
The project provides tests for some plugins, which we can use to ensure
our build configuration hasn't affected the tested functionality.

plugin-test is the only (currently) available test, and adding boost is
a requirement for the tests to be available.

base/install_area_detector.sh
@@ -53,7 +53,8 @@ ln -s RELEASE.local RELEASE_LIBS.local
 echo "
 BUILD_IOCS=NO
 
-WITH_BOOST=NO
+WITH_BOOST=YES
+BOOST_EXTERNAL=YES
 
 WITH_PVA=YES
 WITH_QSRV=YES
@@ -106,6 +107,8 @@ cd -
 
 make -j${JOBS}
 make clean
+
+ADCore/bin/*/plugin-test
 
 cd ..
 

base/Dockerfile
@@ -16,6 +16,8 @@ RUN apt update -y && \
         cmake \
         git \
         libaravis-dev \
+        libboost-test-dev \
+        libboost-system-dev \
         libevent-dev \
         libnet-dev \
         libpcap-dev \

commit 73fbcbab94de9885fbaa46b924954e9b94119e6d

base: run areaDetector plugin-test from /tmp
The plugin-test utility generates some xml and HDF5 files when
performing the tests. When executed from areaDetector root directory,
those files weren't reached by lnls-prune-artifacts and increased the
final image by ~6MB. Run the tests in a temporary directory and clean it
up afterwards.

Fixes: d480b9a (base: run ADCore tests., 2026-06-03)

base/install_area_detector.sh
@@ -108,7 +108,11 @@ cd -
 make -j${JOBS}
 make clean
 
-ADCore/bin/*/plugin-test
+(
+    cd $(mktemp -d)
+    ${EPICS_MODULES_PATH}/areaDetector/ADCore/bin/*/plugin-test
+    rm -rf $PWD
+)
 
 cd ..
 