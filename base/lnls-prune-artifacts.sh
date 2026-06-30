cnpem/epics-in-docker

commit 22ac3c62c9aa53c461024cf5c4d27f4e022c63b4

ioc: prune shared libraries from non-static builds.
Shared libraries may be installed either in /opt or /usr/local/lib but
may not be used by the target binaries from the IOC image. Remove them
during the prune phase before copying both directories to resulting
image to shrink the final image size. Both actual versioned binary and
its symbolic links are removed to keep the filesystem consistent.

When deciding the set of "used libraries", we assume all libraries
inside any APP_DIRS are used. This allows one to specify a dlopen(3)'ed
library path in APP_DIRS, and ensure it is kept in the resulting image.
In addition, paths defined to be kept that affect candidates for removal
are also respected.

Like static libraries, dynamic libraries installed into /usr/local
aren't copied into the final IOC images yet. So adding it into the
argument list doesn't alter the behavior for now, it is simply future
proofing.

base/lnls-prune-artifacts.sh
@@ -46,6 +46,12 @@ find_elf_executables() {
     done < <(find $targets -type f)
 }
 
+find_shared_libraries() {
+    elf_files=$(find_elf_executables $@)
+
+    echo "$elf_files" | grep -E "\.so(.[0-9]+)*$" | sort -u
+}
+
 find_linked_libraries() {
     executables=$(find_elf_executables $@)
 
@@ -151,5 +157,28 @@ remove_static_libraries() {
     done
 }
 
+remove_unused_shared_libraries() {
+    target_libs=$(find_shared_libraries $@)
+    linked_libs=$(find_linked_libraries $@)
+    remove_libs=$(find_shared_libraries /opt /usr/local)
+
+    keep_paths="$target_libs $linked_libs"
+    keep_paths="$keep_paths $(get_defined_paths_to_keep $remove_libs)"
+
+    for lib in $remove_libs; do
+        size=$(du -hs $lib | cut -f 1)
+
+        # if library is not found inside any $keep_paths, remove it
+        if find $keep_paths -path "$lib" -exec false {} + 2> /dev/null; then
+            echo "Removing shared library '$lib' ($size)..."
+
+            rm -f ${lib%.so*}.so*
+        else
+            echo "Keeping shared library '$lib' ($size)..."
+        fi
+    done
+}
+
 remove_unused_epics_modules $@
 remove_static_libraries /opt /usr/local
+remove_unused_shared_libraries $@

commit 7189b7207b62ba33befacbea4a3f3b86b1d49757

base: prune broken symbolic links correctly
Shared libraries may be installed with symbolic links for their version
components, so that build systems can pull the library based on their
requirements on the API. During the prune phase, lnls-prune-artifacts
will get rid of the main shared library as well as its corresponding
links whenever the library isn't used, so the filesystem is consistent
afterward.

That works as expected whenever the user application doesn't link to the
library at all or links to the library using a proper symbolic link for
*.so file. When the project ships the unversioned library as a hard
copy, and such library is used by an application, an unwanted scenario
is reached: remove_unused_shared_libraries() will decide to remove the
*.so copy (because it is an ELF binary, but isn't used at runtime, as
only its versioned copy is) and, thus, it will remove it along with its
versioned friend; although the versioned one is to be kept.

Instead of relying on this fragile heuristics of removing every library
with its corresponding link, which depends on the project build system
to install symbolic links for versioned libraries, only remove the files
checked against the "keep_paths". This ensures we won't ever remove a
shared library in use. Still, to keep the filesystem consistent,
introduce another short pruning step, which targets at finding only
leftover broken links.

Broken links are queried using a GNU-specific flag (-xtype) for find(1),
so that we check the existence of the linked file, not the link itself
[1]. This is not a problem because pruning is relevant only for
Debian-based build images, in which GNU tooling is available. Placing
the documentation comment close to the find(1) would be possible with a
here-string, but it would introduce a final newline [2], which would
require us to treat empty values in the loop. As it is a short loop
body, just use a process substitution instead.

Also, loop with a while-read pattern, so that we properly deal with
files with spaces in their path.

[1] https://unix.stackexchange.com/a/38691
[2] https://www.gnu.org/software/bash/manual/html_node/Redirections.html#Here-Strings

Fixes: 22ac3c6

base/lnls-prune-artifacts.sh
@@ -204,13 +204,24 @@ remove_unused_shared_libraries() {
         if find $keep_paths -path "$lib" -exec false {} + 2> /dev/null; then
             echo "Removing shared library '$lib' ($size)..."
 
-            rm -f ${lib%.so*}.so*
+            rm -f "$lib"
         else
             echo "Keeping shared library '$lib' ($size)..."
         fi
     done
 }
 
+remove_broken_symlinks() {
+    # Depend on the GNU-specific implementation of find(1) to follow the link
+    # before the existence check
+    while read -r link; do
+        echo "Removing broken symlink '$link'..."
+
+        unlink "$link"
+    done < <(find $@ -xtype l)
+}
+
 clean_up_epics_modules $@
 remove_static_libraries /opt /usr/local
 remove_unused_shared_libraries $@
+remove_broken_symlinks /opt /usr/local