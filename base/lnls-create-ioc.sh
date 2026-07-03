b20b200b396f7ccbe97c26a289d5c4a24ba15b0b
Guilherme Rodrigues de Lima <guilherme.lima@lnls.br>
2026-07-03 13:38:13 -0300

    base: include areaDetector CONFIG_SITE when needed
    Automatically include areaDetector's CONFIG_SITE in the generated
    configure/CONFIG_SITE file when creating an areaDetector IOC. This
    provides the necessary definitions for building such IOCs correctly.

--- base/lnls-create-ioc.sh
+++ base/lnls-create-ioc.sh
@@ -97,6 +97,13 @@ echo "
 include \${TOP}/configure/RULES
 " >> "$MAKEFILE"
 
+CONFIG_SITE="configure/CONFIG_SITE"
+
+# Include areaDetector CONFIG_SITE
+if [ "${IS_IOC_AREADETECTOR:-false}" = "true" ]; then
+    echo "include \$(AREA_DETECTOR)/configure/CONFIG_SITE" >> "$CONFIG_SITE"
+fi
+
 # Copy command file(s) or directory(ies)
 if [ -n "${IOC_CMDS:-}" ]; then
     for cmd in $IOC_CMDS; do
