cnpem/epics-in-docker

commit 8ba1cc421ad05b7683f9b2a44253892cef05acd5

ioc: run available IOC tests when building them.
Providing an escape hatch is necessary for IOCs whose tests can't be run
in CI/build environment for some reason. If SKIP_TESTS is set to 1, the
Dockerfile runs "make" instead of "make runtests", which should be fast,
since the actual build has already happened in the previous step.

base/lnls-build-static-ioc.sh
@@ -42,6 +42,10 @@ make_skip() {
 make_skip distclean
 make -j$(nproc)
 
+if [ "$SKIP_TESTS" != 1 ]; then
+  make runtests
+fi
+
 version=$(git describe --tags)
 
 echo $version > $IOC-version

Dockerfile
@@ -70,8 +70,9 @@ FROM build-stage AS dynamic-build
 
 ARG JOBS=1
 ARG RUNDIR
+ARG SKIP_TESTS
 
-RUN make distclean && make -j ${JOBS} && make clean && make -C ${RUNDIR}
+RUN make distclean && make -j ${JOBS} && make $([ "$SKIP_TESTS" != 1 ] && echo runtests) && make clean && make -C ${RUNDIR}
 
 
 FROM base AS dynamic-link
@@ -83,10 +84,11 @@ FROM build-stage AS static-build
 
 ARG JOBS=1
 ARG RUNDIR
+ARG SKIP_TESTS
 
 RUN echo STATIC_BUILD=YES >> configure/CONFIG_SITE
 
-RUN make distclean && make -j ${JOBS} && make clean && make -C ${RUNDIR}
+RUN make distclean && make -j ${JOBS} && make $([ "$SKIP_TESTS" != 1 ] && echo runtests) && make clean && make -C ${RUNDIR}
 
 
 FROM base AS static-link

commit fdb9c3eefda8ba7cc0525c580119e2b4f8a23908

base: fix undefined variable error in lnls-build-static-ioc
Fixes: 8ba1cc4 (ioc: run available IOC tests when building them.,
2024-12-03)

base/lnls-build-static-ioc.sh
@@ -42,7 +42,7 @@ make_skip() {
 make_skip distclean
 make -j$(nproc)
 
-if [ "$SKIP_TESTS" != 1 ]; then
+if [ "${SKIP_TESTS:-}" != 1 ]; then
     make runtests
 fi
 